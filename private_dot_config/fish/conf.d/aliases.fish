53fe0358676d30fa8ccec3cbf8aeeb6e436ae5b6
Zshoham <ShohamZarfti@gmail.com>
2022-01-21 23:01:02 +0200

    moved padding to alacritty and added some aliases

--- private_dot_config/fish/conf.d/aliases.fish
+++ private_dot_config/fish/conf.d/aliases.fish
@@ -1,4 +1,10 @@
 alias cat bat
-alias ls exa
+alias ls 'exa --icons --git'
 alias tree 'exa -T'
 alias files nautilus
+alias dotfiles chezmoi
+
+# ssh
+alias ssh-start 'sudo systemctl start ssh.service'
+alias ssh-stop 'sudo systemctl stop ssh.service'
+alias ssh-status 'sudo systemctl status ssh.service'
