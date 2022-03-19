a1a065afa947259d0f0abd9e0b43aa1a9462c6d8
Shohamza <ShohamZarfti@gmail.com>
2022-03-19 17:54:03 +0200

    added some nix aliases

--- private_dot_config/fish/conf.d/aliases.fish
+++ private_dot_config/fish/conf.d/aliases.fish
@@ -8,3 +8,8 @@ alias dotfiles chezmoi
 alias ssh-start 'sudo systemctl start ssh.service'
 alias ssh-stop 'sudo systemctl stop ssh.service'
 alias ssh-status 'sudo systemctl status ssh.service'
+
+# nix
+alias nix-update 'nix-channel --update; nix-env -u --dry-run'
+alias nix-upgrade 'nix-env -u'
+alias nix-generations 'nix-env --list-generations'
