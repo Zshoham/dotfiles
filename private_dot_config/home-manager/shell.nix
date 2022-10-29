0d7673b023b795e447228358199de4013dc8fadc
Zshoham <ShohamZarfti@gmail.com>
2022-10-29 18:05:34 +0300

    added more shell variables and PATHs

--- private_dot_config/home-manager/shell.nix
+++ private_dot_config/home-manager/shell.nix
@@ -8,17 +8,25 @@
     GNUPGHOME = "${config.xdg.dataHome}/gnupg";
     ZDOTDIR = "${config.xdg.configHome}/zsh";
     LESSHISTFILE = "${config.xdg.stateHome}/less/history";
+    TERMINFO_DIRS = "${config.xdg.dataHome}/terminfo:/usr/share/terminfo";
     IPYTHONDIR = "${config.xdg.configHome}/ipython";
+    SQLITE_HISTORY = "${config.xdg.dataHome}/sqlite_history";
 
     GRADLE_USER_HOME = "${config.xdg.dataHome}/gradle";
+    PIPX_HOME = "${config.xdg.dataHome}/pipx";
     JULIA_DEPOT_PATH = "${config.xdg.dataHome}/julia";
-    CARGO_HOME="${config.xdg.dataHome}/cargo";
-    RUSTUP_HOME="${config.xdg.dataHome}/rustup";
+    CARGO_HOME = "${config.xdg.dataHome}/cargo";
+    RUSTUP_HOME = "${config.xdg.dataHome}/rustup";
+    GOPATH = "${config.xdg.dataHome}/golang";
+    _JAVA_OPTIONS = "-Djava.util.prefs.userRoot=${config.xdg.dataHome}/java";
+    PYENV_ROOTO = "${config.xdg.dataHome}/pyenv";
   };
 
   home.sessionPath = [
     "$CARGO_HOME/bin"
     "$HOME/.local/bin"
+    "$PYENV_ROOT/bin"
+    "$PYENV_ROOT/shims"
   ];
 
   home.shellAliases = {
