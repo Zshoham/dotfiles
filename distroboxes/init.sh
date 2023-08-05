c669f4a1881993f760dbeabc1d4665aa6a87a0f1
Zshoham <ShohamZarfti@gmail.com>
2023-08-05 22:32:16 +0300

    added neovim installation to docker images

--- distroboxes/init.sh
+++ distroboxes/init.sh
@@ -1,7 +1,21 @@
-#/bin/bash
+#!/bin/fish
 
 sudo chown -R $USER:$USER /opt
+
+mv /opt/build_home/config/* $HOME/.config/
+mv /opt/build_home/share/* $HOME/.local/share/
+mv /opt/build_home/state/* $HOME/.local/state/
+mv /opt/build_home/cache/* $HOME/.local/cache/
+
+for path in (find $HOME/.local/ -xtype l)
+  ln -fs "$(readlink $path | sd "/opt/build_home/(?P<dir>)" $HOME'/.local/${dir}')" $path
+end
+
+sd "/opt/build_home/(?P<dir>)" $HOME'/.local/${dir}' (rg -l /opt/build_home $HOME/.local/)
+
 chezmoi init --apply dotfiles/
 
 sudo rm -rf dotfiles/
-sudo rm -- "$0"
+sudo rm build_home.env
+sudo rm -rf build_home/
+sudo rm (status current-filename)
