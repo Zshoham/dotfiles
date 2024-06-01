3c8e3e0eeefb9540eeceadaed9fa7d2d6b257901
Zshoham <ShohamZarfti@gmail.com>
2024-06-01 20:18:42 +0300

    make distrobox init work in existing home folder

--- distroboxes/init.sh
+++ distroboxes/init.sh
@@ -4,11 +4,14 @@ sudo chown -R $USER:$USER /opt
 
 mkdir -p $HOME/.local/bin/ $XDG_CONFIG_HOME $XDG_DATA_HOME $XDG_STATE_HOME $XDG_CACHE_HOME
 
-mv /opt/build_home/config/* $XDG_CONFIG_HOME
-mv /opt/build_home/share/* $XDG_DATA_HOME
-mv /opt/build_home/state/* $XDG_STATE_HOME
-mv /opt/build_home/cache/* $XDG_CACHE_HOME
-mv /opt/build_home/bin/* $HOME/.local/bin/
+sudo cp -a --update=all /opt/build_home/config/ $XDG_CONFIG_HOME
+sudo cp -a --update=all /opt/build_home/share/ $XDG_DATA_HOME
+sudo cp -a --update=all /opt/build_home/state/ $XDG_STATE_HOME
+sudo cp -a --update=all /opt/build_home/cache/ $XDG_CACHE_HOME
+
+if test -d /opt/build_home/bin/
+  sudo cp -a --update=all /opt/build_home/bin/ $HOME/.local/bin/
+end
 
 for path in (fd $HOME/.local/ -t l)
   ln -fs "$(readlink $path | sd "/opt/build_home/(?P<dir>)" $HOME'/.local/${dir}')" $path
