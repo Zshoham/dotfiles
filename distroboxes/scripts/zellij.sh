26fdfaa8c19f1cf91bf88a8fad630c5629fe9fd8
Zshoham <ShohamZarfti@gmail.com>
2024-12-03 22:24:50 +0200

    zellij: added room plugin

--- distroboxes/scripts/zellij.sh
+++ distroboxes/scripts/zellij.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-wget -P $XDG_CONFIG_HOME/zellij/plugins/ https://github.com/dj95/zjstatus/releases/latest/download/zjstatus.wasm
+mkdir -p $XDG_CONFIG_HOME/zellij/plugins/
+curl -L "https://github.com/rvcas/room/releases/latest/download/room.wasm" -o $XDG_CONFIG_HOME/zellij/plugins/room.wasm
+curl -L "https://github.com/dj94/zjstatus/releases/latest/download/zjstatus.wasm" -o $XDG_CONFIG_HOME/zellij/plugins/zjstatus.wasm
+
