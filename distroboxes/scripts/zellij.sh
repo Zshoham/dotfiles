c7c51861ce702a456f056da7eb0b0e2310c76f4d
Zshoham <shohamzarfti@gmail.com>
2024-12-07 19:08:16 +0000

    feat(zellij): add forgot plugin for searching key binds

--- distroboxes/scripts/zellij.sh
+++ distroboxes/scripts/zellij.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 mkdir -p $XDG_CONFIG_HOME/zellij/plugins/
+curl -L "https://github.com/karimould/zellij-forgot/releases/latest/download/zellij_forgot.wasm" -o $XDG_CONFIG_HOME/zellij/plugins/zellij_forgot.wasm
 curl -L "https://github.com/rvcas/room/releases/latest/download/room.wasm" -o $XDG_CONFIG_HOME/zellij/plugins/room.wasm
-curl -L "https://github.com/dj94/zjstatus/releases/latest/download/zjstatus.wasm" -o $XDG_CONFIG_HOME/zellij/plugins/zjstatus.wasm
+curl -L "https://github.com/dj95/zjstatus/releases/latest/download/zjstatus.wasm" -o $XDG_CONFIG_HOME/zellij/plugins/zjstatus.wasm
 
