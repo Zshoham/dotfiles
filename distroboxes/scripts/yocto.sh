e4663a6eb9c59becc359286481ffa66ca1db7c0b
Zshoham <ShohamZarfti@gmail.com>
2025-01-03 15:57:25 +0200

    fix(distroboxes): fix some formatting issues and add work packages

--- distroboxes/scripts/yocto.sh
+++ distroboxes/scripts/yocto.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-pacman -Syyu --noconfirm cpio diffstat
+pacman -Syyu --noconfirm cpio diffstat chrpath rpcsvc-proto
 
-wget -O /opt/yocto.sh https://downloads.yoctoproject.org/releases//yocto/yocto-4.3.4/buildtools/x86_64-buildtools-extended-nativesdk-standalone-4.3.4.sh 
+wget -O /opt/yocto.sh https://downloads.yoctoproject.org/releases/yocto/yocto-4.3.4/buildtools/x86_64-buildtools-extended-nativesdk-standalone-4.3.4.sh 
