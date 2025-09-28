5b8beeaba4ef3aa914920d98875f538b9f5ea680
Zshoham <ShohamZarfti@gmail.com>
2025-09-28 23:13:19 +0300

    feat: install rust-analyzer with rust script

--- distroboxes/scripts/rust.sh
+++ distroboxes/scripts/rust.sh
@@ -2,4 +2,4 @@
 
 export CARGO_HOME="${XDG_DATA_HOME}/cargo/"
 export RUSTUP_HOME="${XDG_DATA_HOME}/rustup/"
-curl https://sh.rustup.rs -sSf | sh -s -- --no-modify-path -y
+curl https://sh.rustup.rs -sSf | sh -s -- --no-modify-path -c rust-analyzer -y
