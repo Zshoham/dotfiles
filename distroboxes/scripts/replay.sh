95c46a54e62d1cd2ed9d7d40d88ed033c13d5fee
Shohamza <ShohamZarfti@gmail.com>
2026-05-14 10:59:11 +0300

    Fix quoting and use TEMP_GIT_DIR when copying files (fix/replay-sh-quoting)

--- distroboxes/scripts/replay.sh
+++ distroboxes/scripts/replay.sh
@@ -4,10 +4,10 @@ TEMP_GIT_DIR=`mktemp -d`
 
 git clone https://github.com/jorgebucaran/replay.fish.git "${TEMP_GIT_DIR}"
 
-mkdir -p "${XDG_CONFIG_HOME}/fish/functions/
-mkdir -p "${XDG_CONFIG_HOME}/fish/completions/
+mkdir -p "${XDG_CONFIG_HOME}/fish/functions/"
+mkdir -p "${XDG_CONFIG_HOME}/fish/completions/"
 
-cp /tmp/replay__fish__repo/functions/* "${XDG_CONFIG_HOME}/fish/functions/"
-cp /tmp/replay__fish__repo/completions/* "${XDG_CONFIG_HOME}/fish/completions/"
+cp "${TEMP_GIT_DIR}/functions/"* "${XDG_CONFIG_HOME}/fish/functions/"
+cp "${TEMP_GIT_DIR}/completions/"* "${XDG_CONFIG_HOME}/fish/completions/"
 
 rm -rf "${TEMP_GIT_DIR}"
