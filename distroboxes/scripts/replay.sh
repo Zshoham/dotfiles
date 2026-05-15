7cdc4b9f36d96f04a5d43eeb69a1531283e87bf5
Zshoham <ShohamZarfti@gmail.com>
2026-05-15 09:38:01 +0000

    fix(basebox): correctly install replay

--- distroboxes/scripts/replay.sh
+++ distroboxes/scripts/replay.sh
@@ -4,10 +4,10 @@ TEMP_GIT_DIR=`mktemp -d`
 
 git clone https://github.com/jorgebucaran/replay.fish.git "${TEMP_GIT_DIR}"
 
-mkdir -p "${XDG_CONFIG_HOME}/fish/functions/
-mkdir -p "${XDG_CONFIG_HOME}/fish/completions/
+mkdir -p "${XDG_CONFIG_HOME}/fish/functions/"
+mkdir -p "${XDG_CONFIG_HOME}/fish/completions/"
 
-cp /tmp/replay__fish__repo/functions/* "${XDG_CONFIG_HOME}/fish/functions/"
-cp /tmp/replay__fish__repo/completions/* "${XDG_CONFIG_HOME}/fish/completions/"
+cp -r "${TEMP_GIT_DIR}/*" "${XDG_CONFIG_HOME}/fish/functions/"
+cp -r "${TEMP_GIT_DIR}/*" "${XDG_CONFIG_HOME}/fish/completions/"
 
 rm -rf "${TEMP_GIT_DIR}"
